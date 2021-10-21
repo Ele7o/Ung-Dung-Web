c8dbdc91e4f6a71e5da16eb491b02da23e8e9466
Erik <superkokono@gmail.com>
2021-10-21 08:05:36 +0700

    v.0.3.4
    Update BT4-bt07

--- BT4/js/bt07.js
+++ BT4/js/bt07.js
@@ -1,25 +1,24 @@
-$(document).ready(function(){
-    $("#dk").click(function(){
-        getInfo();
-    });
-})
+
 function getInfo(){
-    var txtUserName = document.getElementById("txtUserName").value;
-    var txtPassword = document.getElementById("txtPassword").value;
-    var date = new Date($("#txtBirthday").val());
-    var day = date.getDate();
-    var month = date.getMonth() + 1;
-    var year = date.getFullYear();
-    var txtBirthday = day + "/" + month + "/" + year;
-    var txtSexValue = document.getElementsByName('sex');
-    var txtSex;
-    for(i = 0; i <radioValue.length; i++){
-        if(radioValue[i].checked){
-            txtSex = radioValue[i].value;
+
+    var account  = document.getElementById("account").value;
+    var password = document.getElementById("password").value;
+    var sexRadio = document.getElementsByName("sex");
+    var date = document.getElementById("date").value;
+    
+    var interest = document.getElementById("interest").value;
+    var sex = 0;
+    
+    for(var i=0;i<sexRadio.length;i++){
+        if(sexRadio[i].checked){
+            sex = sexRadio[i].value;
         }
     }
-    var selected = document.getElementById("selected").value;
-    // alert("Username: " + txtUserName + "\nPassword: " + txtPassword + "\nBirthday: " + txtBirthday + "\nSex: " + txtSex + "\nSelected: " + selected);
+  
+    document.getElementById("output").innerHTML = "Chào mừng " +account +"\n"
+                                                + "Bạn là "+  sex +"\n"
+                                                + "Mật khẩu của bạn có "+ password.length + " ký tự\n"
+                                                + "Bạn sinh ngày : " + date +"\n"
+                                                + "Vấn đề bạn quan tâm là " + interest;
     
-
 }
